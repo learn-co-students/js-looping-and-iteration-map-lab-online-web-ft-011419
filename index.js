7b444b61deb4f7701cb2703c128e93a52ff17a82
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-25 19:13:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+function lowerCaseDrivers(data){
+  return data.map(function(driver){
+    return   driver.toLowerCase()
+  })
+}
+
+function nameToAttributes(data){
+  return data.map(function (driver){
+    const  newnames = driver.split(" ")
+    return {firstName:  newnames[0] , lastName:newnames[1] };
+  })
+}
+
+function  attributesToPhrase(data){
+  return data.map(function (driver){
+    return `${driver["name"]} is from ${driver["hometown"]}`;
+
+  });
+}
+// function attributesToPhrase(drivers) {
+//   return drivers.map(function(driver) {
+//     return `${driver.name} is from ${driver.hometown}`;
+//   });
+// }
